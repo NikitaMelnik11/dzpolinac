54d023d1bf044503cd2668c56ab755c09be6adb2
Nikita <githu931314@gmail.com>
2025-03-16 00:50:56 +0200

    gotovo

--- obj/Debug/net9.0/dzpolinac.AssemblyInfo.cs
+++ obj/Debug/net9.0/dzpolinac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dzpolinac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9dc09dee5b7f8c886b60fdccfd74dcbc37955b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5f83a9dfbcf00ff14f16c8a25d4b4cd70ff5276")]
 [assembly: System.Reflection.AssemblyProductAttribute("dzpolinac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dzpolinac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
